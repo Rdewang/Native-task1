75f113f879ba8db96eb50f36f0aacfea8729e033
Rdewang <rishabhdewang@citymall.live>
2021-05-09 17:54:56 +0530

    db added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const Knex = require("knex");
 const { Model } = require("objection")
 const knexconfig = require("./knexfile");
+
 // Set up the express app
 const app = express();
 const port = process.env.PORT || 4000;
 const routes = require("./Routes/route")
 const path = require("path");
-require("dotenv");
+require("dotenv").config();
 
 // knex intialization
 const knex = Knex(knexconfig[ process.env.NODE_ENV || 'development']);
@@ -17,7 +18,7 @@ Model.knex(knex);
 
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes)
@@ -27,5 +28,5 @@ app.get('/check', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`, `DB-url = ${process.env.DATABASE_URL}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
